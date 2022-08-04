99fe6ed1cb9b34522feefc27732f9382d098360e
pilar sanchez <mpilar-93@hotmail.com>
2022-08-04 15:44:47 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "json"
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+# movies_array = movies["results"]
+# puts(movies_array)
+
+movies["results"].each do |hash|
+  Movie.create(
+    title: hash["title"],
+    overview: hash["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original" + hash["poster_path"],
+    rating: hash["vote_average"]
+  )
+end
