7f1ca09811ec1ecb78c28cac65ee1ab04ee4fff3
pilar sanchez <mpilar-93@hotmail.com>
2022-08-04 21:57:18 +0100

    started bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "lists#index"
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new"
-  get "lists/:id", to: "lists#show", as: :list
-  post "lists", to: "lists#create"
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id", to: "lists#show", as: :list
+  # post "lists", to: "lists#create"
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :bookmarks_new
+  # post "lists/:id/bookmarks", to: "bookmarks#create", as: :bookmarks
+  # delete "bookmarks/:id", to: "bookmarks#destroy"
 end
