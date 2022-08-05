91fdb6c42157e1317c9340de545e0e4b0491218e
pilar sanchez <mpilar-93@hotmail.com>
2022-08-05 13:07:27 +0100

    sorted out controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only:[:new, :create, :destroy]
+  before_action :set_list, only:[:new, :create]
+  before_action :set_bookmark, only: [:destroy]
 
   def new
     @bookmark = Bookmark.new
@@ -18,6 +19,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
@@ -28,5 +31,9 @@ class BookmarksController < ApplicationController
 
   def set_list
     @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 end
