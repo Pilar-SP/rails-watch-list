50c9f901fbd6a7017d4eae03ed259f6eada9688d
pilar sanchez <mpilar-93@hotmail.com>
2022-08-11 19:37:23 +0100

    awesome seeding with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,39 @@
 require "open-uri"
 require "json"
 
-url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
-movies_serialized = URI.open(url).read
-movies = JSON.parse(movies_serialized)
-# movies_array = movies["results"]
-# puts(movies_array)
+# url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>"
+# movies_serialized = URI.open(url).read
+# movies = JSON.parse(movies_serialized)
+# # movies_array = movies["results"]
+# # puts(movies_array)
 
-movies["results"].each do |hash|
-  Movie.create(
-    title: hash["title"],
-    overview: hash["overview"],
-    poster_url: "https://image.tmdb.org/t/p/original" + hash["poster_path"],
-    rating: hash["vote_average"]
-  )
+# movies["results"].each do |hash|
+#   Movie.create(
+  #     title: hash["title"],
+  #     overview: hash["overview"],
+  #     poster_url: "https://image.tmdb.org/t/p/original" + hash["poster_path"],
+  #     rating: hash["vote_average"]
+  #   )
+  #
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=28c2efab759f272eb65d99fc24fd4a0f&page="
+
+500.times do |i|
+  puts "import movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}#{i + 1}").read)["results"]
+
+  movies.each do |movie|
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "#{base_poster_url}#{movie["backdrop_path"]}",
+      rating: movie["vote_average"]
+    )
+  end
 end
