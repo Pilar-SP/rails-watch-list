1990109ea72789bf151c043acd6861a0cecd338e
pilar sanchez <mpilar-93@hotmail.com>
2022-08-11 20:46:45 +0100

    added break line

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,4 +4,5 @@ class Bookmark < ApplicationRecord
   validates :movie, uniqueness: {scope: :list,
   message: "pair movie/list should have a unique name"}
   validates :comment, length: { minimum: 6 }
+  validates :comment, length: { maximum: 50 }
 end
