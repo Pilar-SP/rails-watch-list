da7959cb40c771fccf31395a3f93bfaecf95bde6
pilar sanchez <mpilar-93@hotmail.com>
2022-08-12 15:36:41 +0100

    added cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = "dnukiyiax"
+  config.api_key = "916156712862556"
+  config.api_secret = "UxGmZqtGpc5TtpJJ8beC5ho8f0k"
+  config.secure = true
+end
