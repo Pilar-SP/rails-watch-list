bf1d20fc78a791b32f7f92287b12bf9b5374d9f0
pilar sanchez <mpilar-93@hotmail.com>
2022-08-17 10:16:56 +0100

    fixed bug after logging in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if user_signed_in?
+      redirect_to lists_path, status: :ok
+    end
+
   end
+
 end
