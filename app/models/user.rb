6863adf22b8e94107d0a3cc015724c436ea2eff4
pilar sanchez <mpilar-93@hotmail.com>
2022-08-18 11:42:19 +0100

    bug in production devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend Devise::Models
+
   has_many :bookmarks, through: :lists
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
