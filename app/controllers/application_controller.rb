0c63da5675f6320b2eee22a16398c38e1e8ccb0d
pilar sanchez <mpilar-93@hotmail.com>
2022-08-18 10:53:23 +0100

    updated logic for unathorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,18 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
 
   #Overwriting the sign_out redirect path method
   def after_sign_in_path_for(resource_or_scope)
     root_path
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 
 end
